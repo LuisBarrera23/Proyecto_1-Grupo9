7644c6ee4e0bed6b3c60beeb1a16476f3eb9548f
Ennox2 <82472966+Ennox2@users.noreply.github.com>
2022-12-15 11:46:57 -0600

    Codigo nuevo 2.0

--- Parqueo1/Parqueo1/Parqueo1.ino
+++ Parqueo1/Parqueo1/Parqueo1.ino
@@ -7,6 +7,20 @@ int R16_9,  R8_1,  A16_9, A8_1,  V16_9,  V8_1 = 0;
 
 
 #define TIEMPO 200
+int inicio = 1;
+
+//LED 1 al 8
+int LED1 = 0;
+int LED2 = 0;
+int LED3 = 0;
+int LED4 = 0;
+int LED5 = 0;
+int LED6 = 0;
+int LED7 = 0;
+int LED8 = 0;
+int LED9 = 0;
+int LED10 = 0;
+
 #define S1 A0
 #define S2 A1
 #define S3 A2
@@ -15,16 +29,11 @@ int R16_9,  R8_1,  A16_9, A8_1,  V16_9,  V8_1 = 0;
 #define S6 A5
 #define S7 A6
 #define S8 A7
-#define S9 A8
-#define S10 A9
-#define S11 A10
-#define S12 A11
-#define S13 A12
-#define S14 A13
-#define S15 A14
+
+
+
 #define S16 A15
 
-<<<<<<< HEAD
 boolean sensor1_old = LOW;
 boolean sensor1_new;
 boolean sensor2_old = LOW;
@@ -55,9 +64,6 @@ boolean sensor14_old = LOW;
 boolean sensor14_new;
 boolean sensor15_old = LOW;
 boolean sensor15_new;
-=======
-
->>>>>>> 425b6d858c8b8dc4dfb0bb4d213488748719b65b
 boolean sensor16_old = LOW;
 boolean sensor16_new;
 
@@ -74,13 +80,122 @@ void ledWrite(){
    
 }
 
+
+void ledPrint(){
+  V8_1 = 0;
+  R8_1 = 0;
+  A8_1 = 0;
+  
+  //LED 1
+  if(LED1 == 0){
+      V8_1 = V8_1 + 1;
+    
+  }else if(LED1 == 1){
+      R8_1 = R8_1 + 1;
+      
+  }else if(LED1 == 2){
+      A8_1 = A8_1 + 1;      
+  }
+
+  //LED 2
+  if(LED2 == 0){
+      V8_1 = V8_1 + 2;
+    
+  }else if(LED2 == 1){
+      R8_1 = R8_1 + 2;
+      
+  }else if(LED2 == 2){
+      A8_1 = A8_1 + 2;      
+  }
+
+  //LED 3
+  if(LED3 == 0){
+      V8_1 = V8_1 + 4;
+    
+  }else if(LED3 == 1){
+      R8_1 = R8_1 + 4;
+      
+  }else if(LED3 == 2){
+      A8_1 = A8_1 + 4;      
+  }
+
+  //LED 4
+  if(LED4 == 0){
+      V8_1 = V8_1 + 8;
+    
+  }else if(LED4 == 1){
+      R8_1 = R8_1 + 8;
+      
+  }else if(LED4 == 2){
+      A8_1 = A8_1 + 8;      
+  }
+
+
+  //LED 5
+  if(LED5 == 0){
+      V8_1 = V8_1 + 16;
+    
+  }else if(LED5 == 1){
+      R8_1 = R8_1 + 16;
+      
+  }else if(LED5 == 2){
+      A8_1 = A8_1 + 16;      
+  }
+
+
+  //LED 6
+  if(LED6 == 0){
+      V8_1 = V8_1 + 32;
+    
+  }else if(LED6 == 1){
+      R8_1 = R8_1 + 32;
+      
+  }else if(LED6 == 2){
+      A8_1 = A8_1 + 32;      
+  }
+
+  //LED 7
+  if(LED7 == 0){
+      V8_1 = V8_1 + 64;
+    
+  }else if(LED7 == 1){
+      R8_1 = R8_1 + 64;
+      
+  }else if(LED7 == 2){
+      A8_1 = A8_1 + 64;      
+  }
+
+
+  //LED 8
+  if(LED8 == 0){
+      V8_1 = V8_1 + 128;
+    
+  }else if(LED8 == 1){
+      R8_1 = R8_1 + 128;
+      
+  }else if(LED8 == 2){
+      A8_1 = A8_1+128;
+  }
+
+  
+
+
+  
+  
+  ledWrite();
+  
+  
+  
+  
+}
+
 void setup(){
   Serial.begin(9600);
   pinMode(entrada, INPUT);
   pinMode(pinData, OUTPUT);
   pinMode(pinLatch, OUTPUT);
   pinMode(pinClock, OUTPUT);
-
+  
   pinMode(S1,INPUT);
   pinMode(S2,INPUT);
   pinMode(S3,INPUT);
@@ -89,55 +204,168 @@ void setup(){
   pinMode(S6,INPUT);
   pinMode(S7,INPUT);
   pinMode(S8,INPUT);
-  pinMode(S9,INPUT);
-  pinMode(S10,INPUT);
-  pinMode(S11,INPUT);
-  pinMode(S12,INPUT);
-  pinMode(S13,INPUT);
-  pinMode(S14,INPUT);
-  pinMode(S15,INPUT);
+  
   pinMode(S16,INPUT);
 
 }
 
 void loop(){
-   if (Serial.available()>0){
+  if (inicio == 1){
+    ledPrint();
+    inicio = 2;
+  }
+
+  if (Serial.available()>0){
     String option = Serial.readString();
-
+    
     if (option != ""){
+      int longitud = option.length() - (0.5 *option.length() - 0.5 );
       s = strtok(option.c_str(), ",");
-      V8_1 = atol(s); 
-      //Serial.println(V8_1);
-      s=strtok(NULL, ",");
-      V16_9 = atol(s);
-      //Serial.println(V16_9);
-
-      s=strtok(NULL, ",");
-      A8_1 = atol(s);
-      //Serial.println(A8_1);
-      s=strtok(NULL, ",");
-      A16_9 = atol(s);
-      //Serial.println(A16_9);
-
-      s=strtok(NULL, ",");
-      R8_1 = atol(s);
-      //Serial.println(R8_1);
-      s=strtok(NULL, ",");
-      R16_9 = atol(s);
-      //Serial.println(R16_9);
       
-      ledWrite(); 
+      for(int i = 0; i<longitud; i++){        
+        if(String(s) == "A"){
+            s=strtok(NULL, ",");
+            LED1 = atol(s);          
+        } 
+        if(String(s) == "B"){
+            s=strtok(NULL, ",");
+            LED2 = atol(s);          
+        }
+        if(String(s) == "C"){
+            s=strtok(NULL, ",");
+            LED3 = atol(s);          
+        }
+        if(String(s) == "D"){
+            s=strtok(NULL, ",");
+            LED4 = atol(s);          
+        }
+        if(String(s) == "E"){
+            s=strtok(NULL, ",");
+            LED5 = atol(s);          
+        }
+        if(String(s) == "F"){
+            s=strtok(NULL, ",");
+            LED6 = atol(s);          
+        }
+        if(String(s) == "G"){
+            s=strtok(NULL, ",");
+            LED7 = atol(s);          
+        }
+        if(String(s) == "H"){
+            s=strtok(NULL, ",");
+            LED8 = atol(s);          
+        }
+        if(String(s) == "I"){
+            s=strtok(NULL, ",");
+            LED9 = atol(s);          
+        }
+        if(String(s) == "J"){
+            s=strtok(NULL, ",");
+            LED10 = atol(s);          
+        } 
+        s=strtok(NULL, ",");
+      }
+      ledPrint(); 
     }
   }
-  sensor16_new = digitalRead(S16);
-<<<<<<< HEAD
-  if((sensor16_new != sensor16_old)){
-    String a="UPDATEP2_PROT," + (String)sensor16_new +",7";
+
+  //LED 1
+  sensor1_new = digitalRead(S1);
+  if((sensor1_new != sensor1_old)){
+    
+    LED1 = sensor1_new;
+    ledPrint();
+    sensor1_old = sensor1_new;
+
+    String a="UPDATEP1_PROT," + (String)sensor1_new +",0";
     Serial.println(a);
-=======
-  if((sensor16_new =! sensor16_old)){
-    Serial.println("{\"sensor\":16}");
->>>>>>> 425b6d858c8b8dc4dfb0bb4d213488748719b65b
-    sensor16_old = sensor16_new;
   }
+
+
+  //LED 2
+  sensor2_new = digitalRead(S2);
+  if((sensor2_new != sensor2_old)){
+    
+    LED2 = sensor2_new;
+    ledPrint();
+    sensor2_old = sensor2_new;
+
+    String a="UPDATEP1_PROT," + (String)sensor2_new +",1";
+    Serial.println(a);
+  }
+
+  //LED 3
+  sensor3_new = digitalRead(S3);
+  if((sensor3_new != sensor3_old)){
+    
+    LED3 = sensor3_new;
+    ledPrint();
+    sensor3_old = sensor3_new;
+
+    String a="UPDATEP1_PROT," + (String)sensor3_new +",2";
+    Serial.println(a);
+  }
+
+  //LED 4
+  sensor4_new = digitalRead(S4);
+  if((sensor4_new != sensor4_old)){
+    
+    LED4 = sensor4_new;
+    ledPrint();
+    sensor4_old = sensor4_new;
+
+    String a="UPDATEP1_PROT," + (String)sensor4_new +",3";
+    Serial.println(a);
+  }
+
+  //LED 5
+  sensor5_new = digitalRead(S5);
+  if((sensor5_new != sensor5_old)){
+    
+    LED5 = sensor5_new;
+    ledPrint();
+    sensor5_old = sensor5_new;
+
+    String a="UPDATEP1_PROT," + (String)sensor5_new +",4";
+    Serial.println(a);
+  }
+
+  //LED 6
+  sensor6_new = digitalRead(S6);
+  if((sensor6_new != sensor6_old)){
+    
+    LED6 = sensor6_new;
+    ledPrint();
+    sensor6_old = sensor6_new;
+
+    String a="UPDATEP1_PROT," + (String)sensor6_new +",5";
+    Serial.println(a);
+  }
+
+  //LED 7
+  sensor7_new = digitalRead(S7);
+  if((sensor7_new != sensor7_old)){
+    
+    LED7 = sensor7_new;
+    ledPrint();
+    sensor7_old = sensor7_new;
+
+    String a="UPDATEP1_PROT," + (String)sensor7_new +",6";
+    Serial.println(a);
+  }
+
+  //LED 8
+  sensor8_new = digitalRead(S8);
+  if((sensor8_new != sensor8_old)){
+    
+    LED8 = sensor8_new;
+    ledPrint();
+    sensor8_old = sensor8_new;
+
+    String a="UPDATEP1_PROT," + (String)sensor8_new +",7";
+    Serial.println(a);
+  }
+
+  
+  
 }
--- Parqueo1/main.py
+++ Parqueo1/main.py
@@ -7,18 +7,37 @@ import json
 from flask import jsonify
 
 s = serial.Serial(port='COM2', baudrate=9600, timeout=200, write_timeout=1)
+estadoLED1_old = "0"
+estadoLED2_old = "0"
+estadoLED3_old = "0"
+estadoLED4_old = "0"
+estadoLED5_old = "0"
+estadoLED6_old = "0"
+estadoLED7_old = "0"
+estadoLED8_old = "0"
+
 
 def hilo():
+    global estadoLED1_old
+    global estadoLED2_old
+    global estadoLED3_old
+    global estadoLED4_old
+    global estadoLED5_old
+    global estadoLED6_old
+    global estadoLED7_old
+    global estadoLED8_old
+
     while(True):
-        url = 'https://arqui1api.000webhostapp.com/base.php'
-        args = {"Methods": "GETP1"}
-
-        response = requests.post(url, json=args)
-
+        
         url1 = 'https://arqui1api.000webhostapp.com/base.php'
         args1 = {"Methods": "GETP2"}
 
-        response1 = requests.post(url1, json=args1)
+        response1 = requests.post(url1, json=args1, timeout=4)
+
+        url = 'https://arqui1api.000webhostapp.com/base.php'
+        args = {"Methods": "GETP1"}
+
+        response = requests.post(url, json=args, timeout=4)
         
 
         if response.status_code == 200 and response1.status_code==200 :
@@ -26,45 +45,83 @@ def hilo():
             payload1=response1.json()
             payload.extend(payload1)
             resultado = payload
+            cadena = ""
+            estadoLED1 = resultado[0].get('state')
 
-            salida1_9r = 0
-            salida9_16r = 0
+            if estadoLED1!=estadoLED1_old:
+                estadoLED1_old = estadoLED1
+                if estadoLED1 == "2":
+                    cadena =  "A,2"
+                    
+            estadoLED2 = resultado[1].get('state')
+            if estadoLED2!=estadoLED2_old:
+                estadoLED2_old = estadoLED2
+                if estadoLED2 == "2":
+                    if(cadena != ""):
+                        cadena += ","
+                    cadena +=  "B,2"
 
-            salida1_9v = 0
-            salida9_16v = 0
+            estadoLED3 = resultado[2].get('state')
+            if estadoLED3!=estadoLED3_old:
+                estadoLED3_old = estadoLED3
+                if estadoLED3 == "2":
+                    if(cadena != ""):
+                        cadena += ","
+                    cadena +=  "C,2"
+                
 
-            salida1_9a = 0
-            salida9_16a = 0
+            estadoLED4 = resultado[3].get('state')
+            if estadoLED4!=estadoLED4_old:
+                estadoLED4_old = estadoLED4
+                if estadoLED4 == "2": 
+                    if(cadena != ""):
+                        cadena += ","
+                    cadena +=  "D,2"
+                
 
-            i = 0
-            y = 0
-            for estado in resultado:
-                r = 2**i
-                if i < 8:
-                    if estado.get('state') == "0":
-                        salida1_9v += r
-                    elif estado.get('state') == "1":
-                        salida1_9r += r
-                    elif estado.get('state') == "2":
-                        salida1_9a += r
-                else:
-                    r = 2**y
-                    if estado.get('state') == "0":
-                        salida9_16v += r
-                    elif estado.get('state') == "1":
-                        salida9_16r += r
-                    elif estado.get('state') == "2":
-                        salida9_16a += r
-                    y += 1
-                i += 1
+            estadoLED5 = resultado[4].get('state')
+            if estadoLED5!=estadoLED5_old:
+                estadoLED5_old = estadoLED5
+                if estadoLED5 == "2":
+                    if(cadena != ""):
+                        cadena += ","
+                    cadena +=  "E,2"
 
-            cadena = str(salida1_9v) + "," + str(salida9_16v) + "," + str(salida1_9a) + \
-                "," + str(salida9_16a) + "," + \
-                str(salida1_9r) + "," + str(salida9_16r)
-            #print("Salida " , cadena)
+            estadoLED6 = resultado[5].get('state')
+            if estadoLED6!=estadoLED6_old:
+                estadoLED6_old = estadoLED6
+                if estadoLED6 == "2" :
+                    if(cadena != ""):
+                        cadena += ","
+                    cadena +=  "F,2"
+                
 
-        s.write(cadena.encode())
-        time.sleep(2)
+            estadoLED7 = resultado[6].get('state')
+            if estadoLED7!=estadoLED7_old:
+                estadoLED7_old = estadoLED7
+                if estadoLED7 == "2": 
+                    if(cadena != ""):
+                        cadena += ","
+                    cadena +=  "G,2"
+                
+
+            estadoLED8 = resultado[7].get('state')
+            if estadoLED8!=estadoLED8_old:
+                estadoLED8_old = estadoLED8
+                if estadoLED8 == "2":
+                    if(cadena != ""):
+                        cadena += ","
+                    cadena +=  "H,2"
+                
+
+            if (cadena != ""):
+                print(cadena)
+                s.write(cadena.encode())
+
+            time.sleep(2)
+            
+                
+        
 
 
 def lectura():
@@ -73,12 +130,9 @@ def lectura():
         try:
             raw_string_b = s.readline()
             raw_string_s = raw_string_b.decode('utf-8')
-            #print(raw_string_s)
+            print(raw_string_s)
             parametros=raw_string_s.split(",")
             url = 'https://arqui1api.000webhostapp.com/base.php'
-            print(parametros[0])
-            print(parametros[1])
-            print(parametros[2])
             args = {"Methods": parametros[0],"estado" : parametros[1],"id":parametros[2]}
 
             response = requests.post(url, json=args)
